6b391892e037574ba906a53d7d6b9910121d1567
isha-web1 <isha.web1@gmail.com>
2023-02-12 12:26:52 +0600

    onclick event added

---
+++ js/event.js
@@ -0,0 +1,21 @@
+// add event listener : 
+// primary button
+function makePink(){
+    document.body.style.backgroundColor = 'Pink';
+    // when click the btn then change h1 text
+    document.getElementById('heading').innerText = 'change the color'
+}
+
+// secondary button
+function makePurple(){
+   document.body.style.backgroundColor = 'purple';
+    // when click the btn then change h1 text
+    document.getElementById('heading').innerText = 'this is purple text'
+}
+
+// success button
+function makeCadetBlue(){
+    document.body.style.backgroundColor = 'cadetblue';
+    // when click the button then change h1 text
+    document.getElementById('heading').innerText = 'make a cadet for this text'
+}
