07d468ac9e5f669c6ad194fd4cf0d7fda40682d7
isha-web1 <isha.web1@gmail.com>
2023-02-12 12:47:44 +0600

    event listener added

--- js/event.js
+++ js/event.js
@@ -1,21 +1,44 @@
-// add event listener : 
+// add event handler : 
 // primary button
 function makePink(){
     document.body.style.backgroundColor = 'Pink';
     // when click the btn then change h1 text
-    document.getElementById('heading').innerText = 'change the color'
+    document.getElementById('heading').innerText = 'this is event handler text-1'
 }
 
 // secondary button
 function makePurple(){
    document.body.style.backgroundColor = 'purple';
     // when click the btn then change h1 text
-    document.getElementById('heading').innerText = 'this is purple text'
+    document.getElementById('heading').innerText = 'this is event handler text-2'
 }
 
 // success button
 function makeCadetBlue(){
     document.body.style.backgroundColor = 'cadetblue';
     // when click the button then change h1 text
-    document.getElementById('heading').innerText = 'make a cadet for this text'
+    document.getElementById('heading').innerText = 'this is event handler text-3'
 }
+
+// now we see even listener :
+// button primary
+const firstEvent = document.getElementById('first').addEventListener('click', function(){
+    document.body.style.backgroundColor = 'olive';
+    // when click the button then change h1 text
+    document.getElementById('heading-2').innerText = ' this is event listener text-1'
+});
+
+// button secondary
+const secondEvent = document.getElementById('second').addEventListener('click', function(){
+    document.body.style.backgroundColor = '#51E1ED';
+    // when click the button then change h1 text
+    document.getElementById('heading-2').innerText = 'this is event listener text-2'
+})
+
+// button success
+const thirdEvent = document.getElementById('third').addEventListener('click', function(){
+    document.body.style.backgroundColor = '#E07C24';
+    // when click the button then change h1 text
+    document.getElementById('heading-2').innerText = 'this is event listener tex-3'
+})
+
