5217cc05408cc0ec9cc55bb9a4879232b5856ac5
isha-web1 <isha.web1@gmail.com>
2023-02-13 12:10:27 +0600

    counter plus-munus added

---
+++ js/count.js
@@ -0,0 +1,28 @@
+// addEventLister by count
+
+
+// let value = 0;
+// document.getElementById('btn-primary').addEventListener('click', function(){
+//     value += 1;
+//     document.getElementById('counter').innerText = value
+// })
+
+// document.getElementById('btn-success').addEventListener('click', function(){
+//     value -= 1;
+//     document.getElementById('counter').innerText = value;
+// })
+
+
+
+// onclick event handler by count
+
+  value = 0;
+function makePlus(){
+    value += 1;
+    document.getElementById('counter').innerText = value;
+};
+  value = 0;
+function makeMinus(){
+    value -= 1;
+    document.getElementById('counter').innerText = value;
+};
